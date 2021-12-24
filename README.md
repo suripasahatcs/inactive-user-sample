ec8dcc1dccf0ebd200db55cfd1536db53a8d56f6
Meiyanathan <meiyanathan@phoenixstudioz.com>
2021-12-24 12:45:29 +0530

    members api call

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # inactive-user-sample
+
+This action retrun users in your organizations.
+## Inputs
+
+## `token`
+
+**Required** PAT token for accesing API. No Default value.
+
+## `orgs-name`
+
+**Required** Organization name for accesing users. No Default value.
+
+## Outputs
+
+## `status`
+
+JSON response.
+
+## Example usage
+
+uses: actions/inactive-user-sample@v1.1
+with:
+  token: 'token'
+  orgs-name: 'internal-test'
---
+++ index.js
@@ -0,0 +1,40 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const axios = require('axios');
+
+
+async function apicall(orgs,PAtoken){
+
+    const config = {
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+        Authorization: 'Bearer ' + PAtoken //the token is a variable which holds the token
+      }
+    }
+    await axios.get(`https://api.github.com/orgs/{orgs}/members`, config)
+    .then(response => {
+      console.log(response)
+      return response;
+    })
+    .catch(error => {
+      console.log(error)
+      return error;
+    })
+
+}
+
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const PAtoken = core.getInput('token');
+  const orgsname = core.getInput('orgs-name');
+  console.log(`Hello ${orgsname}!`);
+  const APIRes = await apicall(orgsname,PAtoken);
+  // const time = (new Date()).toTimeString();
+  core.setOutput("status", APIRes);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
