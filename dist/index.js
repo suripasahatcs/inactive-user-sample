cf5e3b2908c94a6b3653c5ef8c8ee19446ec877d
Meiyanathan <meiyanathan@phoenixstudioz.com>
2021-12-25 23:03:07 +0530

    relase 1.8.3

--- dist/index.js
+++ dist/index.js
@@ -13598,20 +13598,21 @@ async function run() {
 
   console.log(`Attempting to generate organization user activity data, this could take some time...`);
   const userActivity = await orgActivity.getUserActivity(organization, fromDate);
-  saveIntermediateData(outputDir, userActivity.map(activity => activity.jsonPayload));
-
+  // saveIntermediateData(outputDir, userActivity.map(activity => activity.jsonPayload));
+  const jsonresp = userActivity.map(activity => activity.jsonPayload);
+  core.setOutput('report_json', jsonresp);
   // Convert the JavaScript objects into a JSON payload so it can be output
-  console.log(`User activity data captured, generating report...`);
-  const data = userActivity.map(activity => activity.jsonPayload)
-    , csv = json2csv.parse(data, {})
-  ;
+  console.log(`User activity data captured, generating manii report... ${jsonresp}`);
+  // const data = userActivity.map(activity => activity.jsonPayload)
+  //   , csv = json2csv.parse(data, {})
+  // ;
 
-  const file = path.join(outputDir, 'organization_user_activity.csv');
-  fs.writeFileSync(file, csv);
-  console.log(`User Activity Report Generated: ${file}`);
+  // const file = path.join(outputDir, 'organization_user_activity.csv');
+  // fs.writeFileSync(file, csv);
+  // console.log(`User Activity Report Generated: ${file}`);
 
   // Expose the output csv file
-  core.setOutput('report_csv', file);
+  // core.setOutput('report_csv', file);
 }
 
 async function execute() {
